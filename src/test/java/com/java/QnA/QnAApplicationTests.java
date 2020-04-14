d7f9e057a30acbaf511edef130f2cea2eb52b042
TMT <hero219999@gmail.com>
2020-04-14 21:25:01 +0700

    init QnA project

---
+++ src/test/java/com/java/QnA/QnAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.QnA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
